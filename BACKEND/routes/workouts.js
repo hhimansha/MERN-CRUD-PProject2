4409cc8cc44becac00ad9f255c421069403aceed
hhimansha <himansha.official@gmail.com>
2023-11-28 17:48:51 +0530

    second commit

--- BACKEND/routes/workouts.js
+++ BACKEND/routes/workouts.js
@@ -1,29 +1,19 @@
 const express = require('express');
+const {
+    createWorkout,
+    getWorkouts,
+    getWorkout
+} = require('../controllers/workoutController')
 const router = express.Router()
-const Workout = require('../models/workoutModel')
 
 //GET all workouts
-router.get('/', (req, res) =>{
-    res.json({mssg:'GET all workouts'})
-})
+router.get('/', getWorkouts)
 
 //GET a single workout
-router.get('/:id', (req, res) => {
-    res.json({mssg: 'GET a single workout'})
-})
+router.get('/:id', getWorkout)
 
 // POST a new workout
-router.post('/', async (req, res) => {
-    const { title, load, reps } = req.body;
-
-    try {
-        const workout = await Workout.create({ title, load, reps });
-        res.status(200).json(workout);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-
-});
+router.post('/', createWorkout)
 
 
 //DELETE a workout
