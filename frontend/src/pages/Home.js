5f6e91f36e6df5bbbc0a617124fde8ba00adda67
hhimansha <himansha.official@gmail.com>
2023-11-29 13:17:51 +0530

    fetch all data

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,34 @@
-const Home = () => {
-    return(
-        <div className= "home">
-            <h2>Home</h2>
-        </div>
-    )
-}
+import { useEffect, useState } from 'react';
 
-export default Home
+//components
+import WorkoutDetails from '../components/workoutDetails';
+
+const Home = () => {
+    const [workouts, setWorkouts] = useState(null);
+
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch('/api/workouts');
+            const json = await response.json();
+
+            if (response.ok) {
+                setWorkouts(json);
+            }
+        };
+
+        fetchWorkouts();
+    }, []);
+
+    return (
+        <div className="home">
+            <div className="workouts">
+                {workouts &&
+                    workouts.map((workout) => (
+                        <WorkoutDetails key = {workout._id} workout = {workout}/>
+                    ))}
+            </div>
+        </div>
+    );
+};
+
+export default Home;
