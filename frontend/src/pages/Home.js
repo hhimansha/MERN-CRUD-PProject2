f71a1a18fc0bcbe2cb5f61e88869fae361d04138
hhimansha <himansha.official@gmail.com>
2023-11-29 16:00:39 +0530

    real time update

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,13 @@
-import { useEffect, useState } from 'react';
+import { useEffect} from 'react';
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
 
 //components
 import WorkoutDetails from '../components/workoutDetails';
+import WorkoutForm from '../components/WorkoutForm';
 
 const Home = () => {
-    const [workouts, setWorkouts] = useState(null);
+
+    const {workouts, dispatch} = useWorkoutsContext()
 
     useEffect(() => {
         const fetchWorkouts = async () => {
@@ -12,12 +15,12 @@ const Home = () => {
             const json = await response.json();
 
             if (response.ok) {
-                setWorkouts(json);
+                dispatch({type : 'SET_WORKOUTS', payload: json })
             }
         };
 
         fetchWorkouts();
-    }, []);
+    }, [dispatch]);
 
     return (
         <div className="home">
@@ -27,6 +30,7 @@ const Home = () => {
                         <WorkoutDetails key = {workout._id} workout = {workout}/>
                     ))}
             </div>
+            <WorkoutForm />
         </div>
     );
 };
