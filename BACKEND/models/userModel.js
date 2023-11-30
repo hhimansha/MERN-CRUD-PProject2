bdd41d3fb570f63f65ce6c57aa055294659bfaec
hhimansha <himansha.official@gmail.com>
2023-11-30 17:40:15 +0530

    Signing Up & Hashing Passwords

---
+++ BACKEND/models/userModel.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    email: { 
+        type : String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required : true
+    }
+})
+
+//static signup method
+userSchema.statics.signup = async function(email, password){
+    const exists = await this.findOne({email})
+
+    if(exists) {
+        throw Error('Email already in use')
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+     const user = await this.create({email, password : hash})
+
+     return user
+
+}
+
+module.exports = mongoose.model('User', userSchema)
--- BACKEND/server.js
+++ BACKEND/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
 
 require('dotenv').config();
 
@@ -17,6 +18,7 @@ app.use((req, res,next) => {
 
 //routes
 app.use('/api/workouts', workoutRoutes)
+app.use('/api/user', userRoutes)
 
 //connect to DB
 mongoose.connect(process.env.MONG_URL).then(() => {
