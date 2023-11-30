1e2daa5f24c840f0d32e2e3c1417806b8bfd3657
hhimansha <himansha.official@gmail.com>
2023-11-30 17:48:06 +0530

    Email & Password Validation

--- BACKEND/models/userModel.js
+++ BACKEND/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -17,6 +18,19 @@ const userSchema = new Schema({
 
 //static signup method
 userSchema.statics.signup = async function(email, password){
+    
+    //validation
+    if(!email || !password){
+        throw Error('All fields must be filled')
+    }
+    if(!validator.isEmail(email)){
+        throw Error('Email is not valid')
+    }
+    if(!validator.isStrongPassword(password)){
+        throw Error('Password not strong enough')
+    }
+    
+    
     const exists = await this.findOne({email})
 
     if(exists) {
@@ -26,9 +40,10 @@ userSchema.statics.signup = async function(email, password){
     const salt = await bcrypt.genSalt(10)
     const hash = await bcrypt.hash(password, salt)
 
-     const user = await this.create({email, password : hash})
+    const user = await this.create({email, password : hash})
 
-     return user
+
+    return user
 
 }
 
