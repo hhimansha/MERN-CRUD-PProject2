5a1ffae5ec64c34ae4f798a0bbab886abf7c6146
hhimansha <himansha.official@gmail.com>
2023-12-01 16:18:14 +0530

    Making Authorized Requests

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
-
+import {useAuthContext} from '../hooks/useAuthContext'
 
 const WorkoutForm = () => {
     const {dispatch} = useWorkoutsContext()
+    const {user} = useAuthContext()
     
     const [title, setTitle] = useState('')
     const [load, setLoad] = useState('')
@@ -14,13 +15,20 @@ const WorkoutForm = () => {
     const handleSubmit = async (e) => {
         e.preventDefault()
 
+        if(!user){
+            setError('You must be logged in')
+            return
+        }
+
         const workout = {title, load, reps}
 
         const response = await fetch('/api/workouts', {
             method: 'POST',
             body: JSON.stringify(workout),
             headers: {
-                'Content-Type' : 'application/json'
+                'Content-Type' : 'application/json',
+                'Authorization' : `Bearer ${user.token}`
+
             }
         })
 
@@ -44,21 +52,27 @@ const WorkoutForm = () => {
             <h3>Add a New Workout</h3>
 
             <label>Exercise Title : </label>
-            <input type="text" onChange = {(e) => setTitle(e.target.value)}
-            value = {title}
-            className={emptyFields.includes('title') ? 'error' : ''}
+            <input
+                type="text"
+                onChange={(e) => setTitle(e.target.value)}
+                value={title}
+                className={emptyFields && emptyFields.includes('title') ? 'error' : ''}
             />
 
             <label>Load (in kg) : </label>
-            <input type="number" onChange = {(e) => setLoad(e.target.value)}
-            value = {load}
-            className={emptyFields.includes('load') ? 'error' : ''}
+            <input
+                type="number"
+                onChange={(e) => setLoad(e.target.value)}
+                value={load}
+                className={emptyFields && emptyFields.includes('load') ? 'error' : ''}
             />
 
             <label>Reps : </label>
-            <input type="number" onChange = {(e) => setReps(e.target.value)}
-            value = {reps}
-            className={emptyFields.includes('reps') ? 'error' : ''}
+            <input
+                type="number"
+                onChange={(e) => setReps(e.target.value)}
+                value={reps}
+                className={emptyFields && emptyFields.includes('reps') ? 'error' : ''}
             />
 
             <button>Add Workout</button>
--- frontend/src/components/workoutDetails.js
+++ frontend/src/components/workoutDetails.js
@@ -1,13 +1,24 @@
 import {useWorkoutsContext} from '../hooks/useWorkoutsContext'
+import { useAuthContext } from '../hooks/useAuthContext';
 //date fns
 import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 
 const WorkoutDetails = ({ workout }) => {
-    const { dispatch } = useWorkoutsContext();
+    const { dispatch } = useWorkoutsContext()
+    const {user} = useAuthContext()
 
     const handleClick = async () => {
+
+        if(!user){
+            return
+        }
+
         const response = await fetch('/api/workouts/' + workout._id, {
             method: 'DELETE',
+            headers : {
+                'Authorization' : `Bearer ${user.token}`
+
+            }
         });
         const json = await response.json();
 
